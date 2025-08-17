0f328ed6ad2e931a6398d4ead91083eb49edd28a
Esteban Roblero <estebanrolero10@gmail.com>
2025-08-16 20:30:20 -0600

    preguntas actulizada 1

--- preguntas.py
+++ preguntas.py
@@ -1,1 +1,19 @@
+preguntas=[
 
+    #preguntas faciles
+    ("¿El cielo es de color azul?", "si"),
+    ("¿Los perros pueden volar naturalmente", "no")
+    ("¿El hierro flota con el agua?","no"),
+    ("¿ 2+2 es cuatro?", "si"),
+    ("El agua tiene un sabor dulce naturalmente?", "no"),
+    #preguntas medio faciles
+    ("¿Las ranas son anfibios", "si"),
+    ("¿El sonido se propaga más rápido en el agua que en el aire?", "si ")
+    ("Los humanos tienen 5 sentidos principales?", "si"),
+    ("Los rayos son más frios que la luz del sol?", "no"),
+    ("La gravedad funciona igual en la luna que en la tierra?", "no"),
+    #preguntas dificles 
+
+
+
+]

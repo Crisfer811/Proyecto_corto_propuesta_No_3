f9ff831ab2616b9b86105010bf93507b29f1ba85
Crisfer811 <zombiefer15@gmail.com>
2025-08-17 00:40:05 -0600

    Update juego_trivia.py
    Primer commit del programa, llamar los archivos de preguntas y puntajes, asi mismo el for y la funcion que llamaran cada cosa

--- juego_trivia.py
+++ juego_trivia.py
@@ -1,2 +1,16 @@
 import preguntas
 import puntajes
+
+historial_resultados = []
+
+def juego():
+    puntaje_total = 0
+    total = sum(puntajes.puntajes)
+
+    print("----Bienvenido a la trivia de preguntas----\n")
+    print("----A continuacion se le presentaran las pregunta----\n")
+
+    for i, (texto, respuesta_correcta) in enumerate(preguntas.preguntas):
+        valor = puntajes.puntajes[i]
+        print(texto)
+        respuesta_usuario = input("Responde (Si/No): ").lower()

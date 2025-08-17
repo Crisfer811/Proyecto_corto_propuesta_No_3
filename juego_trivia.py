aedfa40ebe253e4280a63d51fe2ea72c18774506
Crisfer811 <zombiefer15@gmail.com>
2025-08-17 15:27:43 -0600

    Update juego_trivia.py
    Continuando con el programa, porque ayer me aburri y no lo acabe, solo falta mostrar los resultados en otra funcion

--- juego_trivia.py
+++ juego_trivia.py
@@ -1,9 +1,11 @@
 import preguntas
 import puntajes
 
+
 def mostrar_bienvenida():
     print("----Bienvenido a la trivia de preguntas----\n")
     print("----A continuacion se le presentaran las preguntas----\n")
+
 
 def preguntas_mostrar(texto, respuesta_correcta, valor):
     print(texto)
@@ -15,4 +17,16 @@ def preguntas_mostrar(texto, respuesta_correcta, valor):
         print("----Respuesta incorrecta----\n")
         print("----Pasando a la siguiente pregunta----\n")
         return 0
-    
+
+
+def jugar_trivia():
+    puntaje_total = 0
+    total = sum(puntajes.puntajes)
+
+    mostrar_bienvenida()
+
+    for i, (texto, respuesta_correcta) in enumerate(preguntas.preguntas):
+        valor = puntajes.puntajes[i]
+        puntaje_total += preguntas_mostrar(texto, respuesta_correcta, valor)
+
+    return puntaje_total, total

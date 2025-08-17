0933a1f56ec928e9e1f4b059e8d7f7c96a9854d0
Esteban Roblero <estebanrolero10@gmail.com>
2025-08-16 21:44:13 -0600

    Preguntas actulizadas 1 terminado

--- preguntas.py
+++ preguntas.py
@@ -2,18 +2,30 @@ preguntas=[
 
     #preguntas faciles
     ("¿El cielo es de color azul?", "si"),
-    ("¿Los perros pueden volar naturalmente", "no")
+    ("¿Los perros pueden volar naturalmente", "no"),
     ("¿El hierro flota con el agua?","no"),
     ("¿ 2+2 es cuatro?", "si"),
     ("El agua tiene un sabor dulce naturalmente?", "no"),
     #preguntas medio faciles
     ("¿Las ranas son anfibios", "si"),
-    ("¿El sonido se propaga más rápido en el agua que en el aire?", "si ")
+    ("¿El sonido se propaga más rápido en el agua que en el aire?", "si "),
     ("Los humanos tienen 5 sentidos principales?", "si"),
     ("Los rayos son más frios que la luz del sol?", "no"),
     ("La gravedad funciona igual en la luna que en la tierra?", "no"),
     #preguntas dificles 
+    ("¿El oxígeno es necesario para la combustión?", "si"),
+    ("¿La luz blanca está compuesta de varios colores?","si"),
+    ("¿El acero es una aleación de hierro y carbono?", "si"),
+    ("¿La fotosíntesis ocurre en las hojas verdes?", "si"),
+    ("¿El agua se convierte en vapor al hervir?","si"),
 
-
-
+    #adicionales
+    ("¿Los tiburones son mamíferos?", "no"),
+    ("¿Los glacíares se derriten por el calor solar?","si"),
+    ("¿El planeta venus tiene una atmósfera más caliente que Mercurio?","si"),
+    ("¿El planeta Marte es más grande que Júpiter?","no"),
+    ("¿Los humanos necesitan oxígeno para vivir?","si"),
 ]
+
+for pregunta, respueta in preguntas:
+    print(pregunta,"-", respueta)

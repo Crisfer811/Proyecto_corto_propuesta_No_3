3585e7cd175c70604f9a180349453df125402385
Crisfer811 <zombiefer15@gmail.com>
2025-08-17 00:46:13 -0600

    Update juego_trivia.py
    Cambiando la estructura de nuevo, no me funciona la anterior, mejor se usaran mas funciones, con una sola esta mas fumado

--- juego_trivia.py
+++ juego_trivia.py
@@ -3,14 +3,6 @@ import puntajes
 
 historial_resultados = []
 
-def juego():
-    puntaje_total = 0
-    total = sum(puntajes.puntajes)
-
+def mostrar_bienvenida():
     print("----Bienvenido a la trivia de preguntas----\n")
-    print("----A continuacion se le presentaran las pregunta----\n")
-
-    for i, (texto, respuesta_correcta) in enumerate(preguntas.preguntas):
-        valor = puntajes.puntajes[i]
-        print(texto)
-        respuesta_usuario = input("Responde (Si/No): ").lower()
+    print("----A continuacion se le presentaran las preguntas----\n")

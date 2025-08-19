ca0490b5579a67ae19041f849718df50a8642e75
Crisfer811 <zombiefer15@gmail.com>
2025-08-18 19:15:37 -0600

    Update preguntas.py
    Cambiando el "SI" y el "NO", por "F" y "V"

--- preguntas.py
+++ preguntas.py
@@ -1,28 +1,28 @@
 preguntas=[
 
     #preguntas faciles
-    ("¿El cielo es de color azul?", "si"),
-    ("¿Los perros pueden volar naturalmente", "no"),
-    ("¿El hierro flota con el agua?","no"),
-    ("¿2+2 es cuatro?", "si"),
-    ("El agua tiene un sabor dulce naturalmente?", "no"),
+    ("¿El cielo es de color azul?", "v"),
+    ("¿Los perros pueden volar naturalmente", "f"),
+    ("¿El hierro flota con el agua?","f"),
+    ("¿2+2 es cuatro?", "v"),
+    ("El agua tiene un sabor dulce naturalmente?", "f"),
     #preguntas medio faciles
-    ("¿Las ranas son anfibios", "si"),
-    ("¿El sonido se propaga más rápido en el agua que en el aire?", "si "),
-    ("Los humanos tienen 5 sentidos principales?", "si"),
-    ("Los rayos son más frios que la luz del sol?", "no"),
-    ("La gravedad funciona igual en la luna que en la tierra?", "no"),
-    #preguntas dificles 
-    ("¿El oxígeno es necesario para la combustión?", "si"),
-    ("¿La luz blanca está compuesta de varios colores?","si"),
-    ("¿El acero es una aleación de hierro y carbono?", "si"),
-    ("¿La fotosíntesis ocurre en las hojas verdes?", "si"),
-    ("¿El agua se convierte en vapor al hervir?","si"),
+    ("¿Las ranas son anfibios", "v"),
+    ("¿El sonido se propaga más rápido en el agua que en el aire?", "v"),
+    ("Los humanos tienen 5 sentidos principales?", "v"),
+    ("Los rayos son más frios que la luz del sol?", "f"),
+    ("La gravedad funciona igual en la luna que en la tierra?", "f"),
+    #preguntas dificles
+    ("¿El oxígeno es necesario para la combustión?", "v"),
+    ("¿La luz blanca está compuesta de varios colores?","v"),
+    ("¿El acero es una aleación de hierro y carbono?", "v"),
+    ("¿La fotosíntesis ocurre en las hojas verdes?", "v"),
+    ("¿El agua se convierte en vapor al hervir?","v"),
 
     #adicionales
-    ("¿Los tiburones son mamíferos?", "no"),
-    ("¿Los glacíares se derriten por el calor solar?","si"),
-    ("¿El planeta venus tiene una atmósfera más caliente que Mercurio?","si"),
-    ("¿El planeta Marte es más grande que Júpiter?","no"),
-    ("¿Los humanos necesitan oxígeno para vivir?","si"),
+    ("¿Los tiburones son mamíferos?", "f"),
+    ("¿Los glacíares se derriten por el calor solar?","v"),
+    ("¿El planeta venus tiene una atmósfera más caliente que Mercurio?","v"),
+    ("¿El planeta Marte es más grande que Júpiter?","f"),
+    ("¿Los humanos necesitan oxígeno para vivir?","v"),
 ]
